adf66e933e4dda58a3d9ca2419b59a839a6b2e64
Daniel Ramirez <zet.daniel@outlook.com>
2024-10-16 15:52:36 -0600

    Initial Commit

---
+++ ProjectOne/ExtensionsCircle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOne
+{
+    public static class ExtensionsCircle
+    {
+        public static double Circumference(this Circle circle)
+        {
+            ArgumentNullException.ThrowIfNull(circle, nameof(circle));
+
+            return circle.Calculate(static r => 2 * Math.PI * Math.Abs(r));
+        }
+    }
+}
